9162bae70351d2bb48ff91684abac5a9d2a7b93c
krzysiekslimak <krzysztof.slimak@outlook.com>
2021-12-04 23:44:01 +0100

    first logic ready

--- battleship.js
+++ battleship.js
@@ -8,18 +8,21 @@ var isSunk = false;
 
 while(isSunk == false){
     guess = prompt("Wybierz cel! (podaj liczbę od 0 do 6)");
-    if (gusess <0 || guess >6) {
+    if (guess < 0 || guess > 6) {
         alert("Proszę podać prawidłowy numer komórki!");
-    }else {
+    }else{
         guesses = guesses + 1;
         if (guess == location1 || guess == location2 || guess == location3){
-            hits = hits + 1
+            alert("Trafiony");
+            hits = hits + 1;
             if (hits == 3) {
                 isSunk = true;
-                alert("Zatopiłeś mój okręt!")
+                alert("Zatopiłeś mój okręt!");
             }
+        }else{
+            alert("Pudło!");
         }
     }
 }
-var stats = "Potrzebowałeś" + guesses + " prób, by zatopiś okręt, " + "czyli Twoja efektywność wynosic:" + (3/guesses) + ".";
+var stats = "Potrzebowałeś " + guesses + " prób, by zatopić okręt, " + "czyli Twoja efektywność wynosi:" + (3/guesses) + ".";
 alert(stats);
