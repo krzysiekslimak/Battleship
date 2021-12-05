ae6d2fb5f4987464e7fc3fa5265e549dc0988238
krzysiekslimak <krzysztof.slimak@outlook.com>
2021-12-05 10:20:27 +0100

    random ship place added

--- battleship.js
+++ battleship.js
@@ -1,10 +1,13 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = logation2 + 1;
 var guess;
 var hits = 0;
 var guesses = 0;
 var isSunk = false; 
+
+
 
 while(isSunk == false){
     guess = prompt("Wybierz cel! (podaj liczbę od 0 do 6)");
