bf82e6a3390826d609ca750bb38fc8683f528435
krzysiekslimak <krzysztof.slimak@outlook.com>
2021-12-17 18:13:21 +0100

    Obiekt widok

--- battleship.js
+++ battleship.js
@@ -1,31 +1,25 @@
-var randomLoc = Math.floor(Math.random() * 5);
-var location1 = randomLoc;
-var location2 = location1 + 1;
-var location3 = location2 + 1;
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSunk = false; 
 
+//Widok//
 
+var view = {
+displayMessage: function(msg){
+var messageArea = document.getElementById("messageArea");
+messageArea.innerHTML = msg;
+},
+displayHit: function(location){
+var cell = document.getElementById(location);
+cell.setAttribute("class", "hit");
 
-while(isSunk == false){
-    guess = prompt("Wybierz cel! (podaj liczbę od 0 do 6)");
-    if (guess < 0 || guess > 6) {
-        alert("Proszę podać prawidłowy numer komórki!");
-    }else{
-        guesses = guesses + 1;
-        if (guess == location1 || guess == location2 || guess == location3){
-            alert("Trafiony");
-            hits = hits + 1;
-            if (hits == 3) {
-                isSunk = true;
-                alert("Zatopiłeś mój okręt!");
-            }
-        }else{
-            alert("Pudło!");
-        }
-    }
+},
+displayMiss: function(location){
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
 }
-var stats = "Potrzebowałeś " + guesses + " prób, by zatopić okręt, " + "czyli Twoja efektywność wynosi:" + (3/guesses) + ".";
-alert(stats);
+};
+    view.displayMiss("00");
+      view.displayHit("34");
+      view.displayMiss("55");
+      view.displayHit("12");
+      view.displayMiss("25");
+      view.displayHit("26");
+      view.displayMessage("Test wyświetlania wiadomości");
